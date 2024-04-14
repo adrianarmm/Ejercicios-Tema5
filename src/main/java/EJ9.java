d8cd4a1a72886ff4fdf63c100185f146ab52323c
adrianarmm <areyemor@myuax.com>
2024-04-14 13:26:50 +0200

    17

--- src/main/java/EJ9.java
+++ src/main/java/EJ9.java
@@ -11,5 +11,16 @@ public class EJ9 {
         String rutaArchivoSalida = "archivoSalida.txt";
         ordenarArchivo(rutaArchivoEntrada, rutaArchivoSalida);
     }
+    private static void ordenarArchivo(String rutaArchivoEntrada, String rutaArchivoSalida)
+    {
+        try (BufferedReader reader = new BufferedReader(new FileReader(rutaArchivoEntrada));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(rutaArchivoSalida)))
+        {
+            List<String> lineas = new ArrayList<>();
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                lineas.add(linea);
+            }
 
-}
+
+        }
