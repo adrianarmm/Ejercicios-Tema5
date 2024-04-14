238921dfd6e929fd5f9806b29739858cce0886c0
adrianarmm <areyemor@myuax.com>
2024-04-14 13:25:49 +0200

    16

--- src/main/java/EJ9.java
+++ src/main/java/EJ9.java
@@ -5,4 +5,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class EJ9 {
+   // PONER ARCHIVOOOOOS
+    public static void main(String[] args) {
+        String rutaArchivoEntrada = "archivoEntrada.txt";
+        String rutaArchivoSalida = "archivoSalida.txt";
+        ordenarArchivo(rutaArchivoEntrada, rutaArchivoSalida);
+    }
+
 }
