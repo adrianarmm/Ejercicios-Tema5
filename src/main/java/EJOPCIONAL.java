87b7ef423a5acf7313e455cee9898b8aa0e99add
adrianarmm <areyemor@myuax.com>
2024-04-14 13:50:12 +0200

    38

--- src/main/java/EJOPCIONAL.java
+++ src/main/java/EJOPCIONAL.java
@@ -2,4 +2,11 @@ import java.util.Arrays;
 
 public class EJOPCIONAL {
 
+    public static void main(String[] args) {
+        int[] array = {10, 5, 3, 8, 2, 6, 4, 7, 9, 1};
+        quicksort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+
 }
