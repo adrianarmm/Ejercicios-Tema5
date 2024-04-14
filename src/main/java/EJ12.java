162b6cb55bc9074edd8fc93439af753a56d69584
adrianarmm <areyemor@myuax.com>
2024-04-14 13:41:04 +0200

    30

--- src/main/java/EJ12.java
+++ src/main/java/EJ12.java
@@ -9,4 +9,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EJ12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<LocalDate> fechas = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy").withResolverStyle(ResolverStyle.STRICT);
+        while (true) {
+            System.out.println("Introduce una fecha (dd/MM/yyyy) o pulse FIN para terminar.");
+            String linea = scanner.nextLine();
+            if (linea.isEmpty()) {
+                break;
+            }
+
 }
